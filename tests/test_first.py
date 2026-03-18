ed3089895402da7db9902fd85679f1821a9d73ac
Edward <edwardpazynets@gmail.com>
2026-03-18 14:45:33 +0200

    mnfctr-2-module-env added login fixture and Faker added dataclass library

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,61 +1,66 @@
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+from tests.conftest import Config
 
-def test_login_with_invalid_credentials(page: Page, configs: dict):
-    open_home_page(page)
+TARGET_PROJECT = "python auto tests"
+
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, configs.email, configs.password)
+
+
+def test_login_with_invalid_credentials(page: Page, configs: Config):
+    open_home_page(page, configs)
 
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
 
     page.get_by_text("Log in", exact=True).click()
 
-    login_user(page, configs["email"], "INVALID_PASSWORD")
+    invalid_password = Faker().password(length=10)
+
+    login_user(page, configs.email, invalid_password)
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
 
 
-def test_search_project_in_company(page: Page, configs: dict):
-    page.goto(configs["login_url"])
-    login_user(page, configs["email"], configs["password"])
+def test_search_project_in_company(page: Page, login):
+    search_for_project(page, TARGET_PROJECT)
 
-    target_project = "python auto tests"
-
-    search_for_project(page, target_project)
-
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
 
 
-def test_open_free_project(page: Page, configs):
-    page.goto(configs["login_url"])
-    login_user(page, configs["email"], configs["password"])
-
+def test_open_free_project(page: Page, login):
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
-    target_project = "python auto tests"
-    search_for_project(page, target_project)
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    search_for_project(page, TARGET_PROJECT)
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
 
 
-def test_create_new_project(page: Page, configs):
-    page.goto(configs["login_url"])
-    login_user(page, configs["email"], configs["password"])
+def test_create_new_project(page: Page, login):
+    fake = Faker()
+    random_title = f"Project {fake.word()}"
 
     page.locator("#content-desktop").get_by_role("link", name="Create", exact=False).click()
-    page.locator("#project_title").fill("test")
+    page.locator("#project_title").fill(random_title)
     page.get_by_role("button", name='Create', exact=True).click()
 
     expect(page.get_by_text("Let's do some testing!")).to_be_visible(timeout=10000)
 
 
-def search_for_project(page: Page, target_project: str):
+def search_for_project(page: Page, TARGET_PROJECT: str):
     expect(page.get_by_role("searchbox", name='Search')).to_be_visible()
-    page.locator("#content-desktop #search").fill(target_project)
+    page.locator("#content-desktop #search").fill(TARGET_PROJECT)
 
 
-def open_home_page(page: Page, configs: dict):
-    page.goto(configs["base_url"])
+def open_home_page(page: Page, configs: Config):
+    page.goto(configs.base_url)
 
 
 def login_user(page: Page, email: str, password: str):
