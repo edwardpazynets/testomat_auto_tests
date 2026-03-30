6f90c36839bd513d76940a31151cb65f336878e0
Edward <edwardpazynets@gmail.com>
2026-03-30 16:25:23 +0300

    mnfctr-3-module-POM added new POM ManageProjects.py refactored old tests

--- tests/web/projects_test.py
+++ tests/web/projects_test.py
@@ -1,16 +1,15 @@
 from faker import Faker
 from playwright.sync_api import Page, expect
 
-from src.web.pages.ProjectDetailPage import ProjectDetailPage
-from src.web.pages.ProjectPage import ProjectPage
+from src.web.pages.CreateNewProjectPage import CreateNewProjectPage
+from src.web.pages.ManageProjects import ManageProjectsPage
+from src.web.pages.ProjectsPage import ProjectsPage
 
 TARGET_PROJECT: str = "python auto tests"
-LABEL_NAME: str = "Auto test"
-TEST_TITLE: str = "auto"
 
 
 def test_search_project_in_company(page: Page, logged_in):
-    project_page = ProjectPage(page)
+    project_page = ProjectsPage(page)
 
     project_page.is_loaded()
     project_page.search_project(TARGET_PROJECT)
@@ -18,47 +17,28 @@ def test_search_project_in_company(page: Page, logged_in):
     expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
 
 
-def test_create_new_project(page: Page, logged_in):
-    project_page = ProjectPage(page)
-    fake = Faker()
-    random_title = f"Project {fake.word()}"
-    project_page.is_loaded()
-    project_page.create_new_project(random_title)
-
-    expect(page.get_by_text("Let's do some testing!")).to_be_visible(timeout=10000)
-
-
 def test_delete_existing_project(page: Page, configs, logged_in):
-    project_page = ProjectPage(page)
+    projects_page = ProjectsPage(page)
+    create_project = CreateNewProjectPage(page)
+    delete_project = ManageProjectsPage(page)
 
     faker = Faker()
     project_to_delete = f"Deleted project {faker.word()}"
 
-    project_page.is_loaded()
-    project_page.create_new_project(project_to_delete)
+    projects_page.is_loaded()
+
+    create_project.open()
+    create_project.is_loaded()
+    create_project.create_new_project(project_to_delete)
+    create_project.click_create()
+
     expect(page.get_by_text("Let's do some testing!")).to_be_visible(timeout=10000)
 
     page.goto(configs.login_url)
-    project_page.open_manage_projects()
+    delete_project.open_manage_projects()
 
     page.on("dialog", lambda dialog: dialog.accept())
-    project_page.delete_existing_project(project_to_delete)
+    delete_project.delete_existing_project(project_to_delete)
 
     page.reload()
     expect(page.get_by_text(project_to_delete)).to_be_hidden(timeout=10000)
-
-
-# created by claude
-def test_filter_by_label(page: Page, configs, logged_in):
-    project_page = ProjectPage(page)
-    detail_page = ProjectDetailPage(page)
-
-    project_page.is_loaded()
-    project_page.search_project(TARGET_PROJECT)
-    page.get_by_role("heading", name=TARGET_PROJECT).click()
-
-    detail_page.is_loaded()
-    detail_page.open_filter()
-    detail_page.select_field_label(LABEL_NAME)
-    detail_page.apply_filter()
-    detail_page.test_visible_by_title(TEST_TITLE)
